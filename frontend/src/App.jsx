30736f8f0f494820a5e49175dd76597432b26d4c
idonotknowwhoiam <marcusymka@gmail.com>
2021-10-09 00:25:17 +0300

    Базовый лейаут с начальной страницей

---
+++ frontend/src/App.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import {
+    View,
+    Panel,
+    PanelHeader,
+    PanelHeaderBack,
+    Group,
+    Placeholder,
+    AppRoot,
+    Epic,
+    Tabbar,
+    TabbarItem,
+} from '@vkontakte/vkui'
+import {
+    Icon28ServicesOutline,
+    Icon28MessageOutline,
+    Icon28MenuOutline,
+    Icon28AddSquareOutline,
+    Icon28UserSquareOutline,
+} from '@vkontakte/icons'
+import '@vkontakte/vkui/dist/vkui.css'
+import AdsView from './views/ads/Ads'
+
+function App() {
+    const [activeStory, setActiveStory] = React.useState('ads')
+    const onStoryChange = (e) => setActiveStory(e.currentTarget.dataset.story)
+
+    return (
+        <AppRoot>
+            <Epic
+                activeStory={activeStory}
+                tabbar={
+                    <Tabbar>
+                        <TabbarItem
+                            onClick={onStoryChange}
+                            selected={activeStory === 'ads'}
+                            data-story="ads"
+                            text="Объявления"
+                        >
+                            <Icon28MenuOutline />
+                        </TabbarItem>
+                        <TabbarItem
+                            onClick={onStoryChange}
+                            selected={activeStory === 'addNew'}
+                            data-story="addNew"
+                            text="Создать"
+                        >
+                            <Icon28AddSquareOutline />
+                        </TabbarItem>
+                        <TabbarItem
+                            onClick={onStoryChange}
+                            selected={activeStory === 'profile'}
+                            data-story="profile"
+                            text="Профиль"
+                        >
+                            <Icon28UserSquareOutline />
+                        </TabbarItem>
+                    </Tabbar>
+                }
+            >
+                <AdsView id="ads" />
+                <View id="addNew" activePanel="addNew">
+                    <Panel id="addNew">
+                        <PanelHeader left={<PanelHeaderBack />}>Сервисы</PanelHeader>
+                        <Group style={{ height: '1000px' }}>
+                            <Placeholder icon={<Icon28ServicesOutline width={56} height={56} />} />
+                        </Group>
+                    </Panel>
+                </View>
+                <View id="profile" activePanel="profile">
+                    <Panel id="profile">
+                        <PanelHeader left={<PanelHeaderBack />}>Сообщения</PanelHeader>
+                        <Group style={{ height: '1000px' }}>
+                            <Placeholder icon={<Icon28MessageOutline width={56} height={56} />} />
+                        </Group>
+                    </Panel>
+                </View>
+            </Epic>
+        </AppRoot>
+    )
+}
+
+export default App
