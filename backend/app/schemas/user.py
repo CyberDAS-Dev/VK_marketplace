86f4166dc323af85d287adf6d5ce2f0fc414cae4
qqpayne <ivanakostelov@gmail.com>
2021-10-20 19:42:43 +0000

    Схемы пользователя и объявления

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import List
+
+from advert import Advert
+from pydantic import BaseModel
+
+
+# Shared properties
+class UserBase(BaseModel):
+    id: int
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    pass
+
+
+class UserInDBBase(UserBase):
+    ads: List[Advert] = []
+
+    class Config:
+        orm_mode = True
+
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    pass
+
+
+# Additional properties stored in DB
+class UserInDB(UserInDBBase):
+    created_at: datetime
