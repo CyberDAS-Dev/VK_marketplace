f1dbc7125f29c253ccf47acdc808616db31e3c3e
qqpayne <ivanakostelov@gmail.com>
2021-10-23 23:22:11 +0000

    Убрал асинхронность логгера и проставил уровень

--- backend/app/middleware/logger.py
+++ backend/app/middleware/logger.py
@@ -18,20 +18,20 @@ class LoggerMiddleware:
             request_id=str(uuid.uuid4()),
             peer=request.client.host,
         )
-        await self.logger.info("request start")
+        self.logger.info("request start")
         start_time = time.time()
 
         try:
             response = await call_next(request)
         except Exception as e:
-            await self.logger.exception(str(e))
+            self.logger.exception(str(e))
             return Response(
                 "Internal Server Error", status_code=500, media_type="text/plain"
             )
 
         process_time = (time.time() - start_time) * 1000
         formatted_process_time = "{0:.2f}".format(process_time)
-        await self.logger.info(
+        self.logger.info(
             "request end",
             status_code=response.status_code,
             completed_in=formatted_process_time,
