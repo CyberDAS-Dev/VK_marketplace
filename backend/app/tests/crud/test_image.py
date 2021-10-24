cd01d4f557abf51771454297d45d8e96a6f33f33
qqpayne <ivanakostelov@gmail.com>
2021-10-24 20:50:27 +0000

    CRUD-операции с изображениями (+ тесты и конфиг)

---
+++ backend/app/tests/crud/test_image.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from PIL import Image
+
+from app import crud
+from app.core.config import settings
+
+
+def test_create_image() -> None:
+    image = Image.open("app/tests/utils/blank_image.jpg")
+    file_name = crud.image.create("blank_image.jpg", image)
+    assert image.fp is None  # убеждаемся, что файл закрыт
+
+    path = Path(f"{settings.IMAGE_DIR}/{file_name}")
+    assert path.exists()
+    path.unlink()
+
+
+def test_remove_image() -> None:
+    image = Image.open("app/tests/utils/blank_image.jpg")
+    file_name = crud.image.create("image_to_delete.jpg", image)
+    path = Path(f"{settings.IMAGE_DIR}/{file_name}")
+    assert path.exists()
+
+    file_name = crud.image.remove("image_to_delete.jpg")
+    assert not path.exists()
