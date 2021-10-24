8cd69ca3860be4013506e030bf07618cd6eaa716
qqpayne <ivanakostelov@gmail.com>
2021-10-24 02:46:30 +0000

    API объявлений (+тесты)

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from .endpoints import users
+from .endpoints import adverts, users
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(adverts.router, prefix="/items", tags=["adverts"])
