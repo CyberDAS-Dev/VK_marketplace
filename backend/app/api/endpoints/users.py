6c55e7ca0c26136e49f266f6e2079ef7543edd09
qqpayne <ivanakostelov@gmail.com>
2021-10-24 02:47:17 +0000

    Асинхронность в API пользователей

--- backend/app/api/endpoints/users.py
+++ backend/app/api/endpoints/users.py
@@ -9,7 +9,9 @@ router = APIRouter()
 
 
 @router.get("/me", response_model=schemas.User)
-def read_user_me(current_user: models.User = Depends(deps.get_current_user)) -> Any:
+async def read_user_me(
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
     """
     Получить текущего пользователя и его объявления.
     """
