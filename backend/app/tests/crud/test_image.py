8dc751406567667757b14b96991b34578889da3b
qqpayne <ivanakostelov@gmail.com>
2021-10-30 08:53:03 +0000

    Улучшил тестовые данные для картинок

--- backend/app/tests/crud/test_image.py
+++ backend/app/tests/crud/test_image.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 
 
 def test_create_image() -> None:
-    image = Image.open("app/tests/utils/blank_image.jpg")
+    image = Image.open("app/tests/data/good_image.jpg")
     file_name = crud.image.create("blank_image.jpg", image)
     assert image.fp is None  # убеждаемся, что файл закрыт
 
@@ -17,7 +17,7 @@ def test_create_image() -> None:
 
 
 def test_remove_image() -> None:
-    image = Image.open("app/tests/utils/blank_image.jpg")
+    image = Image.open("app/tests/data/good_image.jpg")
     file_name = crud.image.create("image_to_delete.jpg", image)
     path = Path(f"{settings.IMAGE_DIR}/{file_name}")
     assert path.exists()
