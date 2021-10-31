dadf1843d2e1c33eda6e25388a2ab4e1141af9d4
qqpayne <ivanakostelov@gmail.com>
2021-10-31 19:49:22 +0300

    Ads -> Feed

--- frontend/src/views/ads/Feed.jsx
+++ frontend/src/views/ads/Feed.jsx
@@ -19,6 +19,7 @@ import {
     Checkbox,
 } from '@vkontakte/vkui'
 import { Icon24Filter, Icon24Dismiss } from '@vkontakte/icons'
+import MainPanel from './panels/Main'
 import logo from '../../images/logo.svg'
 
 export default function AdsView({ id }) {
@@ -184,60 +185,19 @@ export default function AdsView({ id }) {
 
     return (
         <View id={id} activePanel={activePanel} modal={modal}>
-            <Panel id="main">
-                <PanelHeader left={<img src={logo} alt="" />}>Бытовые товары</PanelHeader>
-                <Group>
-                    <Search
-                        after={null}
-                        placeholder="Поиск"
-                        icon={<Icon24Filter />}
-                        onIconClick={() => setActiveModal('categories')}
-                    />
-                    <CardGrid size="l">
-                        <ContentCard
-                            src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-                            subtitle="unsplash"
-                            header="brown and gray mountains under blue sky during daytime photo"
-                            text="Mountain changji"
-                            caption="Photo by Siyuan on Unsplash"
-                            maxHeight={150}
-                            onClick={(e) => console.log(e)}
-                        />
-                        <ContentCard
-                            src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                            subtitle="unsplash"
-                            header="persons left hand with pink paint"
-                            text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
-                            caption="Photo by Alexander Jawfox on Unsplash"
-                            maxHeight={150}
-                        />
-                        <ContentCard
-                            src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                            subtitle="unsplash"
-                            header="persons left hand with pink paint"
-                            text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
-                            caption="Photo by Alexander Jawfox on Unsplash"
-                            maxHeight={150}
-                        />
-                        <ContentCard
-                            src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                            subtitle="unsplash"
-                            header="persons left hand with pink paint"
-                            text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
-                            caption="Photo by Alexander Jawfox on Unsplash"
-                            maxHeight={150}
-                        />
-                        <ContentCard
-                            src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                            subtitle="unsplash"
-                            header="persons left hand with pink paint"
-                            text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
-                            caption="Photo by Alexander Jawfox on Unsplash"
-                            maxHeight={150}
-                        />
-                    </CardGrid>
-                </Group>
-            </Panel>
+            <MainPanel
+                id="main"
+                cardsInfo={[
+                    {
+                        id: '1',
+                        title: 'abc',
+                        description: 'kasd',
+                        cost: 200,
+                        image: 'https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80',
+                    },
+                ]}
+                onSearchClick={() => setActiveModal('categories')}
+            />
         </View>
     )
 }
