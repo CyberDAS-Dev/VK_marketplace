e15d9b4f4c42ab264b4ea18cdebbb86fc30dd1e6
idonotknowwhoiam <marcusymka@gmail.com>
2022-04-25 12:20:56 +0300

    Одна фотография теперь раскрывается в попап

--- frontend/src/views/ads/popouts/MaximizePhoto.jsx
+++ frontend/src/views/ads/popouts/MaximizePhoto.jsx
@@ -3,40 +3,66 @@ import { Gallery, IconButton, PopoutWrapper } from '@vkontakte/vkui'
 import React from 'react'
 
 export default function PhotoPopout({ src, index, closePopout }) {
+    const imgHeight = '70vh'
+
     return (
         <PopoutWrapper alignY="center" alignX="center" onClick={closePopout}>
-            <Gallery
-                styleWidth="100%"
-                bullets="light"
-                initialSlideIndex={index}
-                showArrows
-                style={{ height: '70vh' }}
-            >
-                {src.map((el) => {
-                    return (
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center',
-                                backgroundColor: '#222222',
-                                width: '100%',
-                            }}
-                        >
-                            <img
-                                key={el}
-                                src={el}
-                                alt="Фотография товара"
-                                width="100%"
-                                height="100%"
+            {src.length > 1 ? (
+                <Gallery
+                    styleWidth="100%"
+                    bullets="light"
+                    initialSlideIndex={index}
+                    showArrows
+                    style={{ height: imgHeight }}
+                >
+                    {src.map((el) => {
+                        return (
+                            <div
                                 style={{
-                                    borderRadius: 0,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    backgroundColor: '#222222',
+                                    width: '100%',
                                 }}
-                            />
-                        </div>
-                    )
-                })}
-            </Gallery>
+                            >
+                                <img
+                                    key={el}
+                                    src={el}
+                                    alt="Фотография товара"
+                                    width="100%"
+                                    height="auto"
+                                    style={{
+                                        borderRadius: 0,
+                                    }}
+                                />
+                            </div>
+                        )
+                    })}
+                </Gallery>
+            ) : (
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        backgroundColor: '#222222',
+                        height: imgHeight,
+                        width: '100%',
+                    }}
+                >
+                    <img
+                        key={src}
+                        src={src}
+                        alt="Фотография товара"
+                        width="100%"
+                        height="auto"
+                        style={{
+                            borderRadius: 0,
+                        }}
+                    />
+                </div>
+            )}
 
             <IconButton
                 style={{
