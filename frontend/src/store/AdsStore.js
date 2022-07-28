684bc8785c6d377da9c13d0ba1c36e5bc822ded3
idonotknowwhoiam <marcusymka@gmail.com>
2022-07-28 18:50:44 +0300

    Создал стор для объявлений

---
+++ frontend/src/store/AdsStore.js
@@ -0,0 +1,75 @@
+import { makeAutoObservable } from 'mobx'
+import fetchAdverts from '../api/fetchAdverts'
+
+class Ads {
+    ads = []
+
+    cache = []
+
+    hasMore = false
+
+    isLoading = false
+
+    isError = false
+
+    filters = {
+        category: 'all',
+        type: 'all',
+        sort: 'newer',
+        cost: [0, 2000],
+        dontShowBargain: false,
+        onlyPhoto: false,
+        search: '',
+    }
+
+    constructor() {
+        makeAutoObservable(this, {}, { deep: true })
+    }
+
+    async hasNextPage() {
+        try {
+            const data = await fetchAdverts(this.ads.length, this.filters).catch(() => {
+                this.isError = true
+                this.isLoading = false
+            })
+            if (this.cache.length === 0) this.cache = [...data]
+            if (data.length > 0) {
+                this.hasMore = true
+            } else {
+                this.hasMore = false
+            }
+        } catch (error) {
+            this.isError = true
+        }
+        return this.hasMore
+    }
+
+    async fetchNextAdverts() {
+        this.isLoading = true
+        await this.hasNextPage()
+        if (this.hasMore) {
+            if (this.cache.length > 0) {
+                this.ads = [...this.ads, ...this.cache]
+                this.cache = []
+            } else {
+                const data = await fetchAdverts(this.ads.length, this.filters).catch(() => {
+                    this.isError = true
+                    this.isLoading = false
+                })
+                this.ads = [...this.ads, ...data]
+            }
+        }
+        this.isLoading = false
+    }
+
+    async applyFilters(filters) {
+        this.filters = { ...this.filters, ...filters }
+        this.ads = []
+        this.fetchNextAdverts()
+    }
+}
+
+const ads = new Ads()
+ads.fetchNextAdverts()
+
+export default ads
