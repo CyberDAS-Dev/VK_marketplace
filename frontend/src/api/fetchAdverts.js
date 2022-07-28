4ffca94fbfeab4bf7d8b3f9c51d4220af849747b
idonotknowwhoiam <marcusymka@gmail.com>
2022-07-28 19:48:37 +0300

    К прошлому коммиту, обновил функцию

--- frontend/src/api/fetchAdverts.js
+++ frontend/src/api/fetchAdverts.js
@@ -2,13 +2,13 @@ import http from './http'
 
 const LIMIT = 7
 
-const fetchAdverts = async (skip, filters) => {
+const fetchAdverts = async (skip, filters, limit) => {
     const { category, type, sort, dontShowBargain, cost, onlyPhoto, search } = filters
     const categoryQuery = category === 'all' ? '' : `&category=${category}`
     const [costMin, costMax] = cost
     const typeQuery = type === 'all' ? '' : `&type=${type}`
     const { data } = await http.get(
-        `/items/?limit=${LIMIT}&skip=${skip}${categoryQuery}${typeQuery}
+        `/items/?limit=${limit || LIMIT}&skip=${skip}${categoryQuery}${typeQuery}
 &sort=${sort}&show_bargain=${!dontShowBargain}&cost_min=${costMin}&cost_max=${costMax}&with_photo=${onlyPhoto}
 &search=${search}`
     )
