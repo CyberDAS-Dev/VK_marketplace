25b55f45d6bf6bbaa8e3eeac9f67a20179a18abe
idonotknowwhoiam <marcusymka@gmail.com>
2022-07-31 20:37:34 +0300

    Добавил апи для редактирования объявления, обновил стор

--- frontend/src/api/myAdverts.js
+++ frontend/src/api/myAdverts.js
@@ -2,9 +2,23 @@ import http from '@/api/http'
 
 const LIMIT = 15
 
-const myAdverts = async (skip) => {
+export const myAdverts = async (skip) => {
     const { data } = await http.get(`/users/me/ads?limit=${LIMIT}&skip=${skip}`)
     return data
 }
 
-export default myAdverts
+export const getById = async (id) => {
+    const { data } = await http.get(`/items/${id}`)
+    return data
+}
+
+export const submitAdvert = async (id, ad) => {
+    const { data } = await http.put(`/items/${id}`, {
+        title: ad.title,
+        description: ad.description,
+        cost: ad.cost,
+        bargain: ad.bargain,
+        images: ad.images,
+    })
+    return data
+}
--- frontend/src/views/profile/store/MyAdsStore.js
+++ frontend/src/views/profile/store/MyAdsStore.js
@@ -1,5 +1,5 @@
 import { makeAutoObservable, onBecomeObserved, onBecomeUnobserved } from 'mobx'
-import myAdverts from '@/api/myAdverts'
+import { myAdverts, getById, submitAdvert } from '@/api/myAdverts'
 
 class MyAds {
     ads = []
@@ -7,7 +7,9 @@ class MyAds {
     hasMore = false
     isLoading = false
     isError = false
+    isPrefetching = false
     isPulled = false
+    currentAd = {}
 
     constructor() {
         makeAutoObservable(this, {}, { autoBind: true })
@@ -19,10 +21,13 @@ class MyAds {
     *hasNextPage() {
         const data = yield myAdverts(this.ads.length).catch(() => {
             this.isError = true
-            this.isLoading = false
         })
-        if (this.cache.length === 0) this.cache = [...data]
-        if (data.length > 0) {
+
+        if (data) this.isError = false
+
+        if (data && this.cache.length === 0) this.cache = [...data]
+
+        if (data?.length > 0) {
             this.hasMore = true
         } else {
             this.hasMore = false
@@ -32,21 +37,57 @@ class MyAds {
     }
 
     *fetchNextAdverts() {
-        this.isLoading = true
+        if (this.ads.length === 0) {
+            this.isPrefetching = true
+        } else {
+            this.isLoading = true
+        }
+
         yield this.hasNextPage()
+
         if (this.hasMore) {
             if (this.cache.length > 0) {
                 this.ads = [...this.ads, ...this.cache]
+                this.isError = false
                 this.cache = []
             } else {
                 const data = yield myAdverts(this.ads.length).catch(() => {
                     this.isError = true
-                    this.isLoading = false
                 })
-                this.ads = [...this.ads, ...data]
+                if (data) {
+                    this.ads = [...this.ads, ...data]
+                    this.isError = false
+                }
             }
         }
+
         this.isLoading = false
+        this.isPrefetching = false
+    }
+
+    *editAd(id) {
+        this.isPrefetching = true
+        this.currentAd = {}
+        const current = yield getById(id).catch(() => {
+            this.isError = true
+        })
+
+        if (current) {
+            this.isError = false
+            this.currentAd = current
+        }
+        this.isPrefetching = false
+
+        return this.isError
+    }
+
+    *submitAd(props) {
+        const data = yield submitAdvert(this.currentAd.id, props).catch(() => {
+            this.isError = true
+        })
+
+        if (data) this.isError = false
+        this.currentAd = {}
     }
 
     resetAds() {
