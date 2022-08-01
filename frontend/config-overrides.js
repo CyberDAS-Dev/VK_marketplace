4daa571bb99790c259ea57b55fa63ea20a88e177
idonotknowwhoiam <marcusymka@gmail.com>
2022-08-01 06:33:39 +0300

    Удалил файл прошлой конфигурации

--- frontend/config-overrides.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { aliasWebpack, aliasJest } = require('react-app-alias')
-
-const options = {} // default is empty for most cases
-
-module.exports = aliasWebpack(options)
-module.exports.jest = aliasJest(options)
