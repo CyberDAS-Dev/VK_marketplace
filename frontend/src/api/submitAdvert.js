8b6a8a597f913204e4e7b0bc128850a1e6899a07
idonotknowwhoiam <marcusymka@gmail.com>
2022-08-03 04:52:14 +0300

    Починил загрузку изображений

--- frontend/src/api/submitAdvert.js
+++ frontend/src/api/submitAdvert.js
@@ -1,35 +1,30 @@
 import http from '@/api/http'
-
-// function dataURItoBlob(dataURI) {
-//     // convert base64/URLEncoded data component to raw binary data held in a string
-//     let byteString
-//     if (dataURI.split(',')[0].indexOf('base64') >= 0) byteString = atob(dataURI.split(',')[1])
-//     else byteString = unescape(dataURI.split(',')[1])
-
-//     // separate out the mime component
-//     const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-//     // write the bytes of the string to a typed array
-//     const ia = new Uint8Array(byteString.length)
-//     for (let i = 0; i < byteString.length; i += 1) {
-//         ia[i] = byteString.charCodeAt(i)
-//     }
-
-//     return new Blob([ia], { type: mimeString })
-// }
+import dataURItoBlob from '@/utils/dataURItoBlob'
 
 const submitAdvert = async (props) => {
-    // FIXME не понимаю че апи хочет от меня.. временно передаю пустые картинки
-    // const { images } = props
-    // const imagesUrls = await images.map(async (image) => {
-    //     const formData = new FormData()
-    //     formData.append('file', dataURItoBlob(image.data_url))
-    //     const { data } = await http.post(`/images/`, formData, {
-    //         headers: { 'Content-Type': 'multipart/form-data' },
-    //     })
-    //     return data
-    // })
-    http.post('/items/', { ...props, images: [] })
+    const { images } = props
+
+    // TODO загрузка изображений напрямую в вк
+    // загрузка изображений на свой сервер, возвращает ImageUrl[]
+    try {
+        const imagesUrls = await Promise.all(
+            images.map(async (image) => {
+                const formData = new FormData()
+
+                formData.append('file', dataURItoBlob(image.data_url))
+
+                const { data } = await http.post(`/images/`, formData, {
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                })
+
+                return data
+            })
+        )
+
+        http.post('/items/', { ...props, images: imagesUrls })
+    } catch {
+        throw new Error('Ошибка загрузки изображений')
+    }
 }
 
 export default submitAdvert
